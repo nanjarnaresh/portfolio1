6afc23529c64e8803d0e2d078fb0f9cc1fb24097
nanjarnaresh <nanjarnaresh5@gmail.com>
2026-05-06 18:47:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome Alert
+window.onload = function() {
+  alert("Welcome to my Portfolio ");
+};
+
+// Button Click Example
+function showmessage() {
+  alert("Thanks for visiting my website ");
+}
